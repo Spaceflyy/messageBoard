907c0a13dab27edfeb238689de159f850f27908a
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2024-09-13 18:55:54 +0100

    add form to add new messages

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const express = require("express");
+const path = require("node:path");
+
 const app = express();
 const PORT = 3030;
+
+let messages = [
+	{
+		text: "Hi there!",
+		user: "Amando",
+		added: new Date(),
+	},
+	{
+		text: "Hello World!",
+		user: "Charles",
+		added: new Date(),
+	},
+];
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-	res.send("Hello world");
+	res.render("index", { messages: messages });
+});
+
+app.get("/new", (req, res) => {
+	res.render("form");
+});
+
+app.post("/new", (req, res) => {
+	messages.push({
+		text: req.body.message,
+		user: req.body.user,
+		added: new Date(),
+	});
+	res.redirect("/");
 });
 
 app.listen(PORT, () => {
